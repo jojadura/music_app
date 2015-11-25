0505a3bdd3e79037770fda39e562bca4656ba67d
john j <as@gmail.com>
2015-11-25 17:27:05 -0500

    Implementado los Modelos

---
+++ app/models/album.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: albums
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  image_url   :string
+#  released_at :string
+#  artist_id   :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Album < ActiveRecord::Base
+  belongs_to :artist
+end
