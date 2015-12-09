1a2cdc55e5ee5d59137b837585833edfd9aaa756
john j <as@gmail.com>
2015-12-09 14:34:33 -0500

    Implementa el detalle

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+=begin
   get '/artists', to: 'artists#index'
+  get '/artists/id', to: 'artists#index'
+
+=end
+
+  resources :artists, only: [:index, :show]
 
 end
